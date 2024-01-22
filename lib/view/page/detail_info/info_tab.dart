e17f65baea78084dc6c8db6ea872e58d756728bb
Choi Yena <choiyn33@naver.com>
2024-01-22 22:56:58 +0900

    Design: Refactor UI in Info Tab

--- lib/view/page/detail_info/info_tab.dart
+++ lib/view/page/detail_info/info_tab.dart
@@ -24,106 +24,102 @@ class _InfoTabState extends State<InfoTab> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Column(
-          children: [
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                CarouselSlider.builder(
-                    carouselController: controller,
-                    itemCount: urlImages.length,
-                    itemBuilder: (context, index, realindex) {
-                      final images = urlImages[index];
-                      return buildImage(images, index);
-                    },
-                    options: CarouselOptions(
-                        initialPage: 0,
-                        height: 300,
-                        enableInfiniteScroll: false,
-                        onPageChanged: (index, reason) =>
-                            setState(() => activeIndex = index))),
-                Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(
-                      children: [
-                        if (activeIndex != 0)
-                          IconButton(
-                            icon: const Icon(Icons.arrow_back_ios_rounded),
-                            onPressed: () => {
-                              setState(() {
-                                activeIndex -= 1;
-                                controller.animateToPage(activeIndex);
-                              })
-                            },
-                          ),
-                        const Spacer(),
-                        if (activeIndex != (urlImages.length - 1))
-                          IconButton(
-                            icon: const Icon(Icons.arrow_forward_ios_rounded),
-                            onPressed: () => {
-                              setState(() {
-                                activeIndex += 1;
-                                controller.animateToPage(activeIndex);
-                              })
-                            },
-                          )
-                      ],
-                    ))
-              ],
-            ),
-            const SizedBox(height: 12),
-            buildIndicator(),
-            const SizedBox(height: 12),
-            Container(
-              padding: const EdgeInsets.fromLTRB(20, 12, 20, 12),
-              color: Colors.white,
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "김실종",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: infoItem("성별", "남"),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              CarouselSlider.builder(
+                  carouselController: controller,
+                  itemCount: urlImages.length,
+                  itemBuilder: (context, index, realindex) {
+                    final images = urlImages[index];
+                    return buildImage(images, index);
+                  },
+                  options: CarouselOptions(
+                      initialPage: 0,
+                      height: 300,
+                      enableInfiniteScroll: false,
+                      onPageChanged: (index, reason) =>
+                          setState(() => activeIndex = index))),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    children: [
+                      if (activeIndex != 0)
+                        IconButton(
+                          icon: const Icon(Icons.arrow_back_ios_rounded),
+                          onPressed: () => {
+                            setState(() {
+                              activeIndex -= 1;
+                              controller.animateToPage(activeIndex);
+                            })
+                          },
                         ),
-                        Expanded(
-                          child: infoItem("만나이", "만 9세"),
+                      const Spacer(),
+                      if (activeIndex != (urlImages.length - 1))
+                        IconButton(
+                          icon: const Icon(Icons.arrow_forward_ios_rounded),
+                          onPressed: () => {
+                            setState(() {
+                              activeIndex += 1;
+                              controller.animateToPage(activeIndex);
+                            })
+                          },
                         )
-                      ],
-                    ),
-                    const Divider(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: infoItem("키", "140 cm"),
-                        ),
-                        Expanded(
-                          child: infoItem("몸무게", "30 kg"),
-                        )
-                      ],
-                    ),
-                    const Divider(),
-                    infoItem("마지막 위치", "서울특별시 용산구 청파동 34"),
-                    const Divider(),
-                    infoItem("실종 당시 옷차림",
-                        "바나나 감사합니다 아름드리 아리아 아름드리 도르레 바나나 바나나 나비잠 가온해 가온누리 여우별 별하 나래 로운 별빛 소록소록 나비잠 도서 예그리나 노트북 가온해 "),
-                    const Divider(),
-                    infoItem("특이사항",
-                        "달볓 나래 사과 책방 그루잠 나비잠 그루잠 로운 우리는 나비잠 컴퓨터 나래 도담도담 함초롱하다 달볓 옅구름 소솜 도서관 나비잠 로운 아슬라 도르레 바람꽃 예그리나 예그리나 옅구름 우리는 예그리나 감사합니다 도담도담 이플 포도 곰다시 도서 로운 달볓 안녕 노트북 도담도담 함초롱하다 가온해 예그리나 아리아 비나리 미쁘다 별하 도서관 산들림 감사합니다 그루잠."),
-                    const Divider(),
-                  ]),
-            ),
-          ],
-        ),
-      ],
+                    ],
+                  ))
+            ],
+          ),
+          const SizedBox(height: 12),
+          buildIndicator(),
+          const SizedBox(height: 12),
+          Container(
+            padding: const EdgeInsets.fromLTRB(20, 12, 20, 12),
+            color: Colors.white,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              const Text(
+                "김실종",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: infoItem("성별", "남"),
+                  ),
+                  Expanded(
+                    child: infoItem("만나이", "만 9세"),
+                  )
+                ],
+              ),
+              const Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: infoItem("키", "140 cm"),
+                  ),
+                  Expanded(
+                    child: infoItem("몸무게", "30 kg"),
+                  )
+                ],
+              ),
+              const Divider(),
+              infoItem("마지막 위치", "서울특별시 용산구 청파동 34"),
+              const Divider(),
+              infoItem("실종 당시 옷차림",
+                  "바나나 감사합니다 아름드리 아리아 아름드리 도르레 바나나 바나나 나비잠 가온해 가온누리 여우별 별하 나래 로운 별빛 소록소록 나비잠 도서 예그리나 노트북 가온해 "),
+              const Divider(),
+              infoItem("특이사항",
+                  "달볓 나래 사과 책방 그루잠 나비잠 그루잠 로운 우리는 나비잠 컴퓨터 나래 도담도담 함초롱하다 달볓 옅구름 소솜 도서관 나비잠 로운 아슬라 도르레 바람꽃 예그리나 예그리나 옅구름 우리는 예그리나 감사합니다 도담도담 이플 포도 곰다시 도서 로운 달볓 안녕 노트북 도담도담 함초롱하다 가온해 예그리나 아리아 비나리 미쁘다 별하 도서관 산들림 감사합니다 그루잠."),
+              const Divider(),
+            ]),
+          ),
+        ],
+      ),
     );
   }
 
