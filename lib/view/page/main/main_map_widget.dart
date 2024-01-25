aebc169bba89f15687ad1f7dd47d3722c8af9535
rlaltj <we00425@gmail.com>
2024-01-25 09:34:52 +0900

    Feat: Add Google Map in Map Tab

--- lib/view/page/main/main_map_widget.dart
+++ lib/view/page/main/main_map_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MainMapWidget extends StatefulWidget {
   const MainMapWidget({Key? key}) : super(key: key);
@@ -8,13 +9,29 @@ class MainMapWidget extends StatefulWidget {
 }
 
 class _MainMapWidgetState extends State<MainMapWidget> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(top: 5, bottom: 20),
       child: Column(
         children: [
-          Text('지도 탭 뷰')
+          Expanded(
+            child: GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 11.0,
+              ),
+            ),
+          ),
         ],
       ),
     );
