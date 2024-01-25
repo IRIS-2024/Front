30815626f8bebc1465474e873c9ae0e7308e58cc
rlaltj <we00425@gmail.com>
2024-01-25 19:34:25 +0900

    Feat: Add Validation in Info Form Page

--- lib/view/controller/info_form/info_form_controller.dart
+++ lib/view/controller/info_form/info_form_controller.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:iris_flutter/config/config.dart';
 import 'package:iris_flutter/view/page/Info_form/info_form_page.dart';
 
 class InfoFormController {
@@ -6,16 +8,27 @@ class InfoFormController {
   String dress = '';
   String note = '';
   // 임시
+  RxBool initValidation = true.obs;
   TextEditingController nameController = TextEditingController();
-  Gender selectedGender = Gender.woman;
+  Rx<Gender> selectedGender = Gender.woman.obs;
   TextEditingController ageController = TextEditingController();
   TextEditingController heightController = TextEditingController();
   TextEditingController weightController = TextEditingController();
-  TextEditingController locationController = TextEditingController(text:'위치 입력');
-  bool validateLocation = false;
+  RxString location = Config.enterLocation.obs;
   TextEditingController dressController = TextEditingController();
   TextEditingController noteController = TextEditingController();
 
   // 생성 시간
   // 수정 시간
+
+  registerInfo() {
+    print('print nameControllerText: ${nameController.text}');
+    print('print selectedGender: ${selectedGender.value}');
+    print('print ageControllerText: ${ageController.text}');
+    print('print heightControllerText: ${heightController.text}');
+    print('print weightControllerText: ${weightController.text}');
+    print('print locationControllerText: ${location.value}');
+    print('print dressControllerText: ${dressController.text}');
+    print('print noteController: ${noteController.text}');
+  }
 }
