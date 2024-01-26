7d563cb7416afb9d94332343fbba43ce33f63854
Choi Yena <choiyn33@naver.com>
2024-01-27 02:21:35 +0900

    Test: Google Login Feat

--- lib/view/page/login/login.dart
+++ lib/view/page/login/login.dart
@@ -1,17 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/material.dart';
 
-class Login extends StatefulWidget {
-  const Login({super.key});
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
-  @override
-  State<Login> createState() => _LoginState();
-}
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-class _LoginState extends State<Login> {
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Firebase SNS Login")),
-    );
+        appBar: AppBar(
+          title: const Text("SNS Login"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                  onPressed: signInWithGoogle,
+                  child: const Text("Google Login"))
+            ],
+          ),
+        ));
   }
 }
