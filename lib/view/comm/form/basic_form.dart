33fca5ff63802ed62548e988154c0a435cacac9d
rlaltj <we00425@gmail.com>
2024-01-26 14:53:46 +0900

    Feat: Add Comment Form UI, Validation in Comment Form Page

--- lib/view/comm/form/basic_form.dart
+++ lib/view/comm/form/basic_form.dart
@@ -1,18 +1,24 @@
-
-// form : title + widget
-
 import 'package:flutter/cupertino.dart';
 import 'package:iris_flutter/view/comm/form/form_title.dart';
 
+// form : title + widget
 class BasicForm extends StatelessWidget {
+  final String title;
+  final bool isRequired;
   final Widget widget;
-  const BasicForm({Key? key, required this.widget}) : super(key: key);
+
+  const BasicForm(
+      {Key? key,
+      required this.title,
+      required this.isRequired,
+      required this.widget})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        formTitle(title: '성별', isRequired: true),
+        formTitle(title: title, isRequired: true),
         widget,
         const Padding(padding: EdgeInsets.only(bottom: 15)),
       ],
