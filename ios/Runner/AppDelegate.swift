557ca4d4b47dc5cacb24696166ab5437cac70aa2
rlaltj <we00425@gmail.com>
2024-01-27 16:10:18 +0900

    Fix: Google Map API Setting

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,11 +8,9 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey(Secret().googleMapApiKey)
-    GeneratedPluginRegistrant.register(with: self)
-      
-    GMSServices.provideAPIKey("${GOOGLE_MAP_API_KEY}")
-      
+      GeneratedPluginRegistrant.register(with: self)
+      GMSServices.provideAPIKey("${GOOGLE_MAP_API_KEY}")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
