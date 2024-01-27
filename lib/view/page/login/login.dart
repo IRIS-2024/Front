7ee21f897bde895d69ae5b4125d98324f70771c7
Choi Yena <choiyn33@naver.com>
2024-01-28 02:14:38 +0900

    Design: Login Page

--- lib/view/page/login/login.dart
+++ lib/view/page/login/login.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/material.dart';
+import 'package:iris_flutter/view/comm/custom_appbar.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -26,18 +27,58 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("SNS Login"),
+        body: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Text(
+                "로그인/회원가입",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ]),
+            const SizedBox(
+              height: 48,
+            ),
+            Image.asset(
+              'assets/images/temp_logo.png',
+              height: 200,
+            ),
+            const SizedBox(
+              height: 48,
+            ),
+            SizedBox(
+              width: 300,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: signInWithGoogle,
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15))),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.network(
+                        'http://pngimg.com/uploads/google/google_PNG19635.png',
+                        fit: BoxFit.fitHeight),
+                    const SizedBox(width: 6),
+                    const Text(
+                      "구글로 로그인하기",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                  onPressed: signInWithGoogle,
-                  child: const Text("Google Login"))
-            ],
-          ),
-        ));
+      ),
+    ));
   }
 }
