7f8eb335f82d0436dd6a15df1555d46d8a4e2cab
rlaltj <we00425@gmail.com>
2024-02-13 00:05:40 +0900

    Feat: Add Get Named Routers

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,3 +1,6 @@
+import 'package:get/get.dart';
+import 'package:iris_flutter/view/page/detail_info/info_page.dart';
+
 class Config {
   // info form page
   static bool man = false;
@@ -11,4 +14,13 @@ class Config {
   static String enterTime = '시간 입력';
 
   static int maxImagesLength = 3;
+
+  static final routers = [
+    GetPage(
+        name: routerDetailInfo,
+        page: () => const InfoPage(),
+        transition: Transition.noTransition),
+  ];
+  // routers name
+  static String routerDetailInfo = '/detail_info';
 }
