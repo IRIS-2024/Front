5b830d95761cf33e7c51de26d72d3f3ae3eb06c5
Choi Yena <choiyn33@naver.com>
2024-02-12 18:08:06 +0900

    Feat: Set time format

--- lib/utils/conversion_utils.dart
+++ lib/utils/conversion_utils.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 String combineTimeOfDayWithCurrentDate(TimeOfDay timeOfDay) {
   DateTime now = DateTime.now();
@@ -10,4 +11,14 @@ String combineTimeOfDayWithCurrentDate(TimeOfDay timeOfDay) {
     timeOfDay.minute,
   ).toUtc().toIso8601String();
   return dateTimeString;
+}
+
+String convertDateString(String originalDateString) {
+  // "2024-02-09T07:11:42.069Z";
+
+  DateTime dateTime = DateTime.parse(originalDateString);
+
+  String formattedDateString = DateFormat('yyyy-MM-dd HH:mm').format(dateTime);
+
+  return formattedDateString;
 }
