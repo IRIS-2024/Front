6121d67a22efebf108e12d39f7f051f584f04d39
rlaltj <we00425@gmail.com>
2024-02-17 21:46:01 +0900

    Design: Add Delete Button & API in My Post

--- lib/view/controller/my_page/my_post_controller.dart
+++ lib/view/controller/my_page/my_post_controller.dart
@@ -1,9 +1,11 @@
 import 'dart:developer';
 
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:iris_flutter/config/dio_config.dart';
 import 'package:iris_flutter/model/short_post.dart';
 import 'package:iris_flutter/repository/post_repository.dart';
+import 'package:iris_flutter/view/comm/custom_snackbar.dart';
 
 class MyPostController {
   RxList<ShortPost> postList = <ShortPost>[].obs;
@@ -13,6 +15,18 @@ class MyPostController {
     PostRepository postRepository = PostRepository(dio);
     await postRepository.getPostList(0, 0, 'MINE', 0, 6).then((resp) {
       postList.value = resp;
+    }).catchError((error) {
+      log('[catchError]: $error');
+    });
+  }
+
+  void deletePost(int pid, BuildContext context) async {
+    final dio = createDio();
+    PostRepository postRepository = PostRepository(dio);
+    await postRepository.deletePost(pid).then((resp) {
+      // Error 발생 안 하면 성공
+      customSnackBar(title: '실종 정보 삭제', message: '실종 정보를 삭제하였습니다.', context: context);
+      loadData();
     }).catchError((error) {
       log('[catchError]: $error');
     });
