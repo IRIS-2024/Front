d0d80e0e1697d393f52b201aed87c1be32c19dd5
rlaltj <we00425@gmail.com>
2024-02-22 19:56:55 +0900

    Design: Revise selectedCommtItm BackgroundColor, TabBar BorderRadius in PostPage

--- lib/view/page/post/single_comment_item.dart
+++ lib/view/page/post/single_comment_item.dart
@@ -34,153 +34,156 @@ class _SingleCmtItemState extends State<SingleCmtItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: CustomPadding.pageInsets,
-      child: Column(
-        children: [
-          // 제목
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Flexible(
-                child: Text(
-                  widget.comment.address,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 20),
+    return Container(
+      color: widget.closeAble == true ? Theme.of(context).colorScheme.primaryContainer.withOpacity(0.1) : Colors.white,
+      child: Padding(
+        padding: CustomPadding.pageInsets.copyWith(top: 16),
+        child: Column(
+          children: [
+            // 제목
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  child: Text(
+                    widget.comment.address,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
                 ),
-              ),
-              Text(getTimeDifference(widget.comment.createdAt),
-                  style: const TextStyle(fontSize: 12)),
-              if (widget.closeAble == true)
-                IconButton(
-                    onPressed: widget.closeMethod,
-                    icon: const Icon(Icons.close)),
-              if (widget.closeAble != true && widget.comment.author) // 댓글 삭제
-                IconButton(
-                    onPressed: () {
-                      Get.put(PostController())
-                          .deleteComment(widget.comment.cid, context);
-                    },
-                    icon: const Icon(Icons.delete_outline)),
-            ],
-          ),
-          const Padding(padding: CustomPadding.regularBottom),
-          // 내용
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if (widget.comment.images.isNotEmpty)
-                Column(
-                  children: [
-                    widget.hasImgAuth
-                        ? GestureDetector(
-                            onTap: () {
-                              showDialog<String>(
-                                context: context,
-                                builder: (BuildContext context) =>
-                                    imgDialog(context),
-                              );
-                            },
-                            child: Container(
-                              // 이미지(썸네일)
-                              constraints: BoxConstraints(
-                                  maxWidth: MediaQuery.of(context).size.width),
-                              width: 100,
-                              child: Stack(
-                                children: [
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(10),
-                                    child: Image.network(
-                                      widget.comment.images[0],
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                  Positioned(
-                                    top: 8,
-                                    right: 8,
-                                    child: Container(
-                                      padding: const EdgeInsets.all(4),
-                                      decoration: BoxDecoration(
-                                        color: Colors.black.withOpacity(0.6),
-                                        borderRadius: BorderRadius.circular(10),
+                Text(getTimeDifference(widget.comment.createdAt),
+                    style: const TextStyle(fontSize: 12)),
+                if (widget.closeAble == true)
+                  IconButton(
+                      onPressed: widget.closeMethod,
+                      icon: const Icon(Icons.close)),
+                if (widget.closeAble != true && widget.comment.author) // 댓글 삭제
+                  IconButton(
+                      onPressed: () {
+                        Get.put(PostController())
+                            .deleteComment(widget.comment.cid, context);
+                      },
+                      icon: const Icon(Icons.delete_outline)),
+              ],
+            ),
+            const Padding(padding: CustomPadding.regularBottom),
+            // 내용
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (widget.comment.images.isNotEmpty)
+                  Column(
+                    children: [
+                      widget.hasImgAuth
+                          ? GestureDetector(
+                              onTap: () {
+                                showDialog<String>(
+                                  context: context,
+                                  builder: (BuildContext context) =>
+                                      imgDialog(context),
+                                );
+                              },
+                              child: Container(
+                                // 이미지(썸네일)
+                                constraints: BoxConstraints(
+                                    maxWidth: MediaQuery.of(context).size.width),
+                                width: 100,
+                                child: Stack(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: Image.network(
+                                        widget.comment.images[0],
+                                        fit: BoxFit.cover,
                                       ),
-                                      child: Text(
-                                        "${widget.comment.images.length}",
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.bold,
+                                    ),
+                                    Positioned(
+                                      top: 8,
+                                      right: 8,
+                                      child: Container(
+                                        padding: const EdgeInsets.all(4),
+                                        decoration: BoxDecoration(
+                                          color: Colors.black.withOpacity(0.6),
+                                          borderRadius: BorderRadius.circular(10),
+                                        ),
+                                        child: Text(
+                                          "${widget.comment.images.length}",
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                          ),
                                         ),
                                       ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ),
-                          )
-                        : Container(
-                            constraints: BoxConstraints(
-                                maxWidth: MediaQuery.of(context).size.width),
-                            width: 100,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: ImageFiltered(
-                                imageFilter:
-                                    ImageFilter.blur(sigmaX: 4.5, sigmaY: 4.5),
-                                child: Image.network(
-                                  widget.comment.images[0],
-                                  fit: BoxFit.cover,
+                            )
+                          : Container(
+                              constraints: BoxConstraints(
+                                  maxWidth: MediaQuery.of(context).size.width),
+                              width: 100,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: ImageFiltered(
+                                  imageFilter:
+                                      ImageFilter.blur(sigmaX: 4.5, sigmaY: 4.5),
+                                  child: Image.network(
+                                    widget.comment.images[0],
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                    const Padding(padding: CustomPadding.slimBottom),
-                    Text('일치율 ${widget.comment.accuracy!.toStringAsFixed(0)} %',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            color: Theme.of(context).colorScheme.primary))
-                  ],
+                      const Padding(padding: CustomPadding.slimBottom),
+                      Text('일치율 ${widget.comment.accuracy!.toStringAsFixed(0)} %',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Theme.of(context).colorScheme.primary))
+                    ],
+                  ),
+                const SizedBox(width: 12),
+                Expanded(
+                  // 상세 정보 글
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      if (widget.comment.clothes != '')
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text("옷차림",
+                                style: TextStyle(color: Colors.grey)),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            Flexible(child: Text(widget.comment.clothes!))
+                          ],
+                        ),
+                      if (widget.comment.details != '')
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text("당시 상황",
+                                style: TextStyle(color: Colors.grey)),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            Flexible(child: Text(widget.comment.details!))
+                          ],
+                        ),
+                    ],
+                  ),
                 ),
-              const SizedBox(width: 12),
-              Expanded(
-                // 상세 정보 글
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    if (widget.comment.clothes != '')
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text("옷차림",
-                              style: TextStyle(color: Colors.grey)),
-                          const SizedBox(
-                            width: 8,
-                          ),
-                          Flexible(child: Text(widget.comment.clothes!))
-                        ],
-                      ),
-                    if (widget.comment.details != '')
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text("당시 상황",
-                              style: TextStyle(color: Colors.grey)),
-                          const SizedBox(
-                            width: 8,
-                          ),
-                          Flexible(child: Text(widget.comment.details!))
-                        ],
-                      ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
